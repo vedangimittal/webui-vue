8fd2bdb4367266eb38cffc7bec24c952c8fb1213
Dixsie Wolmers <Dixsie@ibm.com>
2021-10-19 20:20:12 -0500

    Move TPM policy to Policies page (#15)
    - TPM policy was removed from server power ops boot settings
    section and moved to Policies page.
    - Uses new Redfish endpoint
    - Refactored policy page to include aria-labelledby and
    aria-describedby for each toggle
    - Refactored toast messages
    - Removed unused translations from en-US for policy page
    
    Upstream: https://gerrit.openbmc-project.xyz/c/openbmc/webui-vue/+/46960
    
    TODO: Add upstream once hardcoded endpoints are resolved.
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>

--- src/store/modules/Operations/BootSettingsStore.js
+++ src/store/modules/Operations/BootSettingsStore.js
@@ -10,44 +10,30 @@ const BootSettingsStore = {
       'pvm_default_os_type',
       'pvm_rpa_boot_mode',
       'pvm_os_boot_type',
+      'pvm_sys_dump_active',
     ],
     attributeValues: null,
     automaticRetryConfigValue: '',
     biosAttributes: null,
-    bootSourceOptions: [],
-    bootSource: null,
     bootFault: '',
-    overrideEnabled: null,
     powerRestorePolicyValue: '',
   },
   getters: {
     attributeValues: (state) => state.attributeValues,
     automaticRetryConfigValue: (state) => state.automaticRetryConfigValue,
     biosAttributes: (state) => state.biosAttributes,
-    bootSourceOptions: (state) => state.bootSourceOptions,
-    bootSource: (state) => state.bootSource,
-    overrideEnabled: (state) => state.overrideEnabled,
-    powerRestorePolicyValue: (state) => state.powerRestorePolicyValue,
     bootFaultValue: (state) => state.bootFault,
+    powerRestorePolicyValue: (state) => state.powerRestorePolicyValue,
+    systemDumpActive: (state) =>
+      state.biosAttributes?.pvm_sys_dump_active === 'Enabled',
   },
   mutations: {
     setAttributeValues: (state, attributeValues) =>
       (state.attributeValues = attributeValues),
     setBiosAttributes: (state, biosAttributes) =>
       (state.biosAttributes = biosAttributes),
-    setBootSourceOptions: (state, bootSourceOptions) =>
-      (state.bootSourceOptions = bootSourceOptions),
-    setBootSource: (state, bootSource) => (state.bootSource = bootSource),
     setStopBootOnFaultValue: (state, bootFault) =>
       (state.bootFault = bootFault),
-    setOverrideEnabled: (state, overrideEnabled) => {
-      if (overrideEnabled === 'Once') {
-        state.overrideEnabled = true;
-      } else {
-        // 'Continuous' or 'Disabled'
-        state.overrideEnabled = false;
-      }
-    },
     setPowerRestorePolicyValue: (state, powerRestorePolicyValue) =>
       (state.powerRestorePolicyValue = powerRestorePolicyValue),
     setAutomaticRetryConfigValue: (state, automaticRetryConfigValue) =>
@@ -64,54 +50,9 @@ const BootSettingsStore = {
         })
         .catch((error) => console.log(error));
     },
-    async getBootSettings({ commit }) {
-      return await api
-        .get('/redfish/v1/Systems/system')
-        .then(({ data: { Boot } }) => {
-          commit(
-            'setBootSourceOptions',
-            Boot['BootSourceOverrideTarget@Redfish.AllowableValues']
-          );
-          commit('setOverrideEnabled', Boot.BootSourceOverrideEnabled);
-          commit('setBootSource', Boot.BootSourceOverrideTarget);
-        })
-        .catch((error) => console.log(error));
-    },
-    saveBootSettings({ commit, dispatch }, { bootSource, overrideEnabled }) {
-      const data = { Boot: {} };
-      data.Boot.BootSourceOverrideTarget = bootSource;
-      if (overrideEnabled) {
-        data.Boot.BootSourceOverrideEnabled = 'Once';
-      } else if (bootSource === 'None') {
-        data.Boot.BootSourceOverrideEnabled = 'Disabled';
-      } else {
-        data.Boot.BootSourceOverrideEnabled = 'Continuous';
-      }
-      return api
-        .patch('/redfish/v1/Systems/system', data)
-        .then((response) => {
-          // If request success, commit the values
-          commit('setBootSource', data.Boot.BootSourceOverrideTarget);
-          commit('setOverrideEnabled', data.Boot.BootSourceOverrideEnabled);
-          return response;
-        })
-        .catch((error) => {
-          console.log(error);
-          // If request error, GET saved options
-          dispatch('getBootSettings');
-          return error;
-        });
-    },
-    async saveSettings(
-      { dispatch },
-      { bootSource, overrideEnabled, biosSettings }
-    ) {
+    async saveSettings({ dispatch }, { biosSettings }) {
       const promises = [];
-      if (bootSource || overrideEnabled) {
-        promises.push(
-          dispatch('saveBootSettings', { bootSource, overrideEnabled })
-        );
-      }
+
       if (biosSettings) {
         promises.push(dispatch('saveBiosSettings', biosSettings));
       }
@@ -131,11 +72,11 @@ const BootSettingsStore = {
         })
       );
     },
-    async getBiosAttributes({ commit }) {
+    async getBiosAttributes({ commit, state }) {
       return await api
         .get('/redfish/v1/Systems/system/Bios')
         .then(({ data: { Attributes } }) => {
-          const filteredAttribute = this.state.serverBootSettings.attributeKeys
+          const filteredAttributes = state.attributeKeys
             .filter((key) => Object.keys(Attributes).includes(key))
             .reduce((obj, key) => {
               return {
@@ -143,11 +84,11 @@ const BootSettingsStore = {
                 [key]: Attributes[key],
               };
             }, {});
-          commit('setBiosAttributes', filteredAttribute);
+          commit('setBiosAttributes', filteredAttributes);
         })
         .catch((error) => console.log(error));
     },
-    async getAttributeValues({ commit }) {
+    async getAttributeValues({ commit, state }) {
       return await api
         .get(
           '/redfish/v1/Registries/BiosAttributeRegistry/BiosAttributeRegistry'
@@ -158,17 +99,20 @@ const BootSettingsStore = {
               RegistryEntries: { Attributes },
             },
           }) => {
-            //Array for state BIOS attributes is created
-            const filteredAttributeValues = this.state.serverBootSettings.attributeKeys
+            // Array for state BIOS attributes is created
+            const filteredAttributeValues = state.attributeKeys
               .reduce((arr, attriValue) => {
                 return [
                   ...arr,
-                  ...Attributes.filter(
-                    (value) => attriValue === value.AttributeName
-                  ),
+                  ...Attributes.filter((value) => {
+                    return (
+                      attriValue !== 'pvm_sys_dump_active' &&
+                      attriValue === value.AttributeName
+                    );
+                  }),
                 ];
               }, [])
-              //Array of objects with the key as value and text is created
+              // Array of objects with the key as value and text is created
               .reduce((obj, attributeObj) => {
                 return {
                   ...obj,
@@ -209,6 +153,21 @@ const BootSettingsStore = {
         .catch((error) => {
           console.log(error);
           return error;
+        });
+    },
+    async standbyToRuntime() {
+      return await api
+        .post('redfish/v1/Systems/hypervisor/Actions/ComputerSystem.Reset', {
+          ResetType: 'On',
+        })
+        .then(() => {
+          return i18n.tc('pageServerPowerOperations.toast.successSaveSettings');
+        })
+        .catch((error) => {
+          console.log(error);
+          throw new Error(
+            i18n.tc('pageServerPowerOperations.toast.errorSaveSettings')
+          );
         });
     },
     saveOperatingModeSettings({ dispatch }) {
--- src/views/Operations/ServerPowerOperations/BootSettings.vue
+++ src/views/Operations/ServerPowerOperations/BootSettings.vue
@@ -1,37 +1,8 @@
 <template>
   <b-form novalidate @submit.prevent="handleSubmit">
-    <b-row>
-      <b-col sm="6">
-        <b-form-group
-          :label="
-            $t('pageServerPowerOperations.bootSettings.bootSettingsOverride')
-          "
-          label-for="boot-option"
-          class="mb-3"
-        >
-          <b-form-select
-            id="boot-option"
-            v-model="form.bootOption"
-            :disabled="bootSourceOptions.length === 0"
-            :options="bootSourceOptions"
-            @change="onChangeSelect"
-          >
-          </b-form-select>
-        </b-form-group>
-        <b-form-checkbox
-          v-model="form.oneTimeBoot"
-          class="mb-4"
-          :disabled="form.bootOption === 'None'"
-          @change="$v.form.oneTimeBoot.$touch()"
-        >
-          {{ $t('pageServerPowerOperations.bootSettings.enableOneTimeBoot') }}
-        </b-form-checkbox>
-      </b-col>
-    </b-row>
     <bios-settings
       v-if="form.attributes && form.attributeValues"
       :key="componentKey"
-      :attributes="form.attributes"
       :attribute-values="form.attributeValues"
       @updated-attributes="updateAttributeKeys"
     />
@@ -53,8 +24,6 @@ export default {
     return {
       componentKey: 0,
       form: {
-        bootOption: this.$store.getters['serverBootSettings/bootSource'],
-        oneTimeBoot: this.$store.getters['serverBootSettings/overrideEnabled'],
         attributes: this.$store.getters['serverBootSettings/biosAttributes'],
         attributeValues: this.$store.getters[
           'serverBootSettings/attributeValues'
@@ -63,13 +32,7 @@ export default {
     };
   },
   computed: {
-    ...mapState('serverBootSettings', [
-      'attributeValues',
-      'biosAttributes',
-      'bootSourceOptions',
-      'bootSource',
-      'overrideEnabled',
-    ]),
+    ...mapState('serverBootSettings', ['attributeValues', 'biosAttributes']),
   },
   watch: {
     attributeValues: function (value) {
@@ -77,20 +40,6 @@ export default {
     },
     biosAttributes: function (value) {
       this.form.attributes = value;
-    },
-    bootSource: function (value) {
-      this.form.bootOption = value;
-    },
-    overrideEnabled: function (value) {
-      this.form.oneTimeBoot = value;
-    },
-  },
-  validations: {
-    // Empty validations to leverage vuelidate form states
-    // to check for changed values
-    form: {
-      bootOption: {},
-      oneTimeBoot: {},
     },
   },
   created() {
@@ -107,19 +56,9 @@ export default {
     },
     handleSubmit() {
       this.startLoader();
-      const bootSettingsChanged =
-        this.$v.form.bootOption.$dirty || this.$v.form.oneTimeBoot.$dirty;
       let settings;
-      let bootSource = null;
-      let overrideEnabled = null;
       let biosSettings = this.form.attributes;
-      if (bootSettingsChanged) {
-        // If bootSource or overrideEnabled changed get
-        // both current values to send with request
-        bootSource = this.form.bootOption;
-        overrideEnabled = this.form.oneTimeBoot;
-      }
-      settings = { bootSource, overrideEnabled, biosSettings };
+      settings = { biosSettings };
       this.$store
         .dispatch('serverBootSettings/saveSettings', settings)
         .then((message) => {
@@ -128,14 +67,8 @@ export default {
         })
         .catch(({ message }) => this.errorToast(message))
         .finally(() => {
-          this.$v.form.$reset();
           this.endLoader();
         });
-    },
-    onChangeSelect(selectedOption) {
-      this.$v.form.bootOption.$touch();
-      // Disable one time boot if selected boot option is 'None'
-      if (selectedOption === 'None') this.form.oneTimeBoot = false;
     },
   },
 };
