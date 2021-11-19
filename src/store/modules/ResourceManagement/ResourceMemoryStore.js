55bbe0a0aeca06523a391120c426b04f8ead3dac
Sandeepa Singh <Sandeepa.Singh@ibm.com>
2021-11-19 20:02:18 +0530

    Memory-I/O-Adapter-Enlarged-Capacity (#21)
    This section will let the user update the slot count for Node 0
    
    Signed-off-by: Sandeepa Singh sandeepa.singh@ibm.com

--- src/store/modules/ResourceManagement/ResourceMemoryStore.js
+++ src/store/modules/ResourceManagement/ResourceMemoryStore.js
@@ -6,16 +6,20 @@ const ResourceMemoryStore = {
   state: {
     logicalMemorySizeOptions: [],
     logicalMemorySize: null,
+    ioAdapterCapacity: null,
   },
   getters: {
     logicalMemorySizeOptions: (state) => state.logicalMemorySizeOptions,
     logicalMemorySize: (state) => state.logicalMemorySize,
+    ioAdapterCapacity: (state) => state.ioAdapterCapacity,
   },
   mutations: {
     setLogicalMemorySizeOptions: (state, logicalMemorySizeOptions) =>
       (state.logicalMemorySizeOptions = logicalMemorySizeOptions),
     setLogicalMemorySize: (state, logicalMemorySize) =>
       (state.logicalMemorySize = logicalMemorySize),
+    setIoAdapterCapacity: (state, ioAdapterCapacity) =>
+      (state.ioAdapterCapacity = ioAdapterCapacity),
   },
   actions: {
     async getMemorySizeOptions({ commit }) {
@@ -41,6 +45,49 @@ const ResourceMemoryStore = {
           commit('setLogicalMemorySize', hb_memory_region_size)
         )
         .catch((error) => console.log(error));
+    },
+    async getIoAdapterCapacity({ commit }) {
+      return await api
+        .get(
+          '/redfish/v1/Registries/BiosAttributeRegistry/BiosAttributeRegistry'
+        )
+        .then(({ data: { RegistryEntries } }) => {
+          const ioAdapterCapacity = RegistryEntries.Attributes.filter(
+            (Attribute) =>
+              Attribute.AttributeName == 'hb_ioadapter_enlarged_capacity'
+          );
+          let ioEnlargedAdapterCapacity = ioAdapterCapacity[0].CurrentValue;
+          commit('setIoAdapterCapacity', ioEnlargedAdapterCapacity);
+        })
+        .catch((error) => console.log(error));
+    },
+    async saveEnlargedCapacity({ commit }) {
+      const updatedIoEnlargedCapacity = {
+        Attributes: {
+          hb_ioadapter_enlarged_capacity: this.state.resourceMemory
+            .ioAdapterCapacity,
+        },
+      };
+      return await api
+        .patch(
+          '/redfish/v1/Systems/system/Bios/Settings',
+          updatedIoEnlargedCapacity
+        )
+        .then(() => {
+          commit(
+            'setIoAdapterCapacity',
+            updatedIoEnlargedCapacity.Attributes.hb_ioadapter_enlarged_capacity
+          );
+          return i18n.t(
+            'pageMemory.toast.successSavingAdapterEnlargedCapacity'
+          );
+        })
+        .catch((error) => {
+          console.log('error', error);
+          throw new Error(
+            i18n.t('pageMemory.toast.errorSavingAdapterEnlargedCapacity')
+          );
+        });
     },
     async saveSettings({ commit }, logicalMemorySize) {
       const updatedMemorySize = {
--- src/views/ResourceManagement/Memory/Memory.vue
+++ src/views/ResourceManagement/Memory/Memory.vue
@@ -13,25 +13,79 @@
         </alert>
       </b-col>
     </b-row>
-    <page-section :section-title="$t('pageMemory.logicalMemorySize')">
+    <page-section
+      :section-title="$t('pageMemory.logicalMemorySize')"
+      class="mb-4"
+    >
+      <b-row>
+        <b-col md="8" xl="6">
+          <p>{{ $t('pageMemory.logicalMemorySizeHeading') }}</p>
+        </b-col>
+      </b-row>
       <b-row>
         <b-col md="8" xl="6">
           <b-form novalidate @submit.prevent="handleSubmit">
             <b-form-group
-              :label="$t('pageMemory.logicalMemorySizeHeading')"
+              :label="$t('pageMemory.memoryBlockSize')"
               label-for="logical-memory-size-option"
-              class="mb-3 w-75"
+              class="mb-3"
             >
               <b-form-select
                 id="logical-memory-size-option"
                 v-model="form.logicalMemorySizeOption"
-                class="w-75"
                 :options="logicalMemorySizeOptions"
               >
               </b-form-select>
             </b-form-group>
             <b-button variant="primary" type="submit" class="mt-3 mb-3">
               {{ $t('pageMemory.updateLogicalMemorySize') }}
+            </b-button>
+          </b-form>
+        </b-col>
+      </b-row>
+    </page-section>
+    <div class="section-divider mb-3 mt-3"></div>
+    <page-section
+      :section-title="$t('pageMemory.ioApdapterEnlargedCapacityTitle')"
+    >
+      <b-row>
+        <b-col md="8" xl="6">
+          <p>{{ $t('pageMemory.ioApdapterEnlargedCapacity') }}</p>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="8" xl="6">
+          <b-form @submit.prevent="updateAdapterCapacity()">
+            <b-form-group
+              :label="$t('pageMemory.slotCountForNode0')"
+              label-for="io-adapter-enlarged-capacity"
+              class="mb-3"
+            >
+              <b-form-input
+                id="input-io-adapter-capacity"
+                v-model.number="ioAdapterCapacity"
+                data-test-id="io-adapter-capacity"
+                type="number"
+                :state="getValidationState($v.ioAdapterCapacity)"
+              ></b-form-input>
+              <b-form-invalid-feedback role="alert">
+                <template
+                  v-if="
+                    !$v.ioAdapterCapacity.minLength ||
+                    !$v.ioAdapterCapacity.maxLength
+                  "
+                >
+                  {{
+                    $t('global.form.valueMustBeBetween', {
+                      min: 0,
+                      max: 255,
+                    })
+                  }}
+                </template>
+              </b-form-invalid-feedback>
+            </b-form-group>
+            <b-button variant="primary" type="submit" class="mt-3 mb-3">
+              {{ $t('pageMemory.updateIoAdapterEnlargedCapacity') }}
             </b-button>
           </b-form>
         </b-col>
@@ -47,11 +101,13 @@ import BVToastMixin from '@/components/Mixins/BVToastMixin';
 import LoadingBarMixin from '@/components/Mixins/LoadingBarMixin';
 import PageTitle from '@/components/Global/PageTitle';
 import PageSection from '@/components/Global/PageSection';
+import { minValue, maxValue } from 'vuelidate/lib/validators';
+import VuelidateMixin from '@/components/Mixins/VuelidateMixin.js';
 
 export default {
   name: 'Memory',
   components: { Alert, PageSection, PageTitle },
-  mixins: [BVToastMixin, LoadingBarMixin],
+  mixins: [VuelidateMixin, BVToastMixin, LoadingBarMixin],
   data() {
     return {
       form: {
@@ -66,6 +122,15 @@ export default {
       'logicalMemorySizeOptions',
       'logicalMemorySize',
     ]),
+    ioAdapterCapacity: {
+      get() {
+        return this.$store.getters['resourceMemory/ioAdapterCapacity'];
+      },
+      set(value) {
+        this.$v.$touch();
+        this.$store.commit('resourceMemory/setIoAdapterCapacity', value);
+      },
+    },
   },
   watch: {
     logicalMemorySize: function (value) {
@@ -78,12 +143,17 @@ export default {
     form: {
       logicalMemorySizeOption: {},
     },
+    ioAdapterCapacity: {
+      minValue: minValue(0),
+      maxValue: maxValue(255),
+    },
   },
   created() {
     this.startLoader();
     Promise.all([
       this.$store.dispatch('resourceMemory/getMemorySizeOptions'),
       this.$store.dispatch('resourceMemory/getLogicalMemorySize'),
+      this.$store.dispatch('resourceMemory/getIoAdapterCapacity'),
     ]).finally(() => this.endLoader());
   },
   methods: {
@@ -92,6 +162,17 @@ export default {
       let logicalMemorySize = this.form.logicalMemorySizeOption;
       this.$store
         .dispatch('resourceMemory/saveSettings', logicalMemorySize)
+        .then((message) => this.successToast(message))
+        .catch(({ message }) => this.errorToast(message))
+        .finally(() => {
+          this.$v.form.$reset();
+          this.endLoader();
+        });
+    },
+    updateAdapterCapacity() {
+      this.startLoader();
+      this.$store
+        .dispatch('resourceMemory/saveEnlargedCapacity')
         .then((message) => this.successToast(message))
         .catch(({ message }) => this.errorToast(message))
         .finally(() => {
