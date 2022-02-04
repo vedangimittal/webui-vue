34944932563ac391dd1836d765306ae8d97f64e5
A Nikhil <A.Nikhil@ibm.com>
2022-02-04 21:58:24 +0530

    Overview page missing info - Operating mode (#44)
    Added the Operating mode under Server Information in the Overview page
    
    Signed-off-by: Nikhil Ashoka <a.nikhil@ibm.com>

--- src/views/Overview/OverviewServer.vue
+++ src/views/Overview/OverviewServer.vue
@@ -4,12 +4,18 @@
     :to="`/hardware-status/inventory`"
   >
     <b-row class="mt-3">
-      <b-col lg="6">
+      <b-col sm="6" lg="6">
         <dl>
           <dt>{{ $t('pageOverview.model') }}</dt>
           <dd>{{ dataFormatter(serverModel) }}</dd>
           <dt>{{ $t('pageOverview.serialNumber') }}</dt>
           <dd>{{ dataFormatter(serverSerialNumber) }}</dd>
+        </dl>
+      </b-col>
+      <b-col sm="6" lg="6">
+        <dl>
+          <dt>{{ $t('pageOverview.operatingMode') }}</dt>
+          <dd>{{ dataFormatter(operatingMode) }}</dd>
         </dl>
       </b-col>
     </b-row>
@@ -30,18 +36,23 @@ export default {
   computed: {
     ...mapState({
       server: (state) => state.system.systems[0],
+      biosAttributes: (state) => state.serverBootSettings.biosAttributes,
       serverModel() {
         return this.server?.model;
       },
       serverSerialNumber() {
         return this.server?.serialNumber;
       },
+      operatingMode() {
+        return this.biosAttributes?.pvm_system_operating_mode;
+      },
     }),
   },
   created() {
-    this.$store.dispatch('system/getSystem').finally(() => {
-      this.$root.$emit('overview-server-complete');
-    });
+    this.$store.dispatch('serverBootSettings/getBiosAttributes'),
+      this.$store.dispatch('system/getSystem').finally(() => {
+        this.$root.$emit('overview-server-complete');
+      });
   },
 };
 </script>
