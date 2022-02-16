1c3ed0a6333243e3bd62be7a18943829b30c5f03
A Nikhil <A.Nikhil@ibm.com>
2022-02-16 19:48:24 +0530

    Update bmc manager properties (#63)
    - Removed irrelevant properties
    - Rearranged required properties to be displayed
      under bmc manager in Inventory page
    
    Signed-off-by: Nikhil Ashoka <a.nikhil@ibm.com>

--- src/views/HardwareStatus/Inventory/InventoryTableBmcManager.vue
+++ src/views/HardwareStatus/Inventory/InventoryTableBmcManager.vue
@@ -49,11 +49,11 @@
       <template #row-details="{ item }">
         <b-container fluid>
           <b-row>
-            <b-col class="mt-2" sm="6" xl="6">
+            <b-col class="mt-2">
               <dl>
-                <!-- Name -->
-                <dt>{{ $t('pageInventory.table.name') }}:</dt>
-                <dd>{{ dataFormatter(item.name) }}</dd>
+                <!-- Description -->
+                <dt>{{ $t('pageInventory.table.description') }}:</dt>
+                <dd>{{ dataFormatter(item.description) }}</dd>
                 <!-- Part number -->
                 <dt>{{ $t('pageInventory.table.partNumber') }}:</dt>
                 <dd>{{ dataFormatter(item.partNumber) }}</dd>
@@ -64,94 +64,8 @@
                 <dt>{{ $t('pageInventory.table.sparePartNumber') }}:</dt>
                 <dd>{{ dataFormatter(item.sparePartNumber) }}</dd>
                 <!-- Model -->
-                <dt>{{ $t('pageInventory.table.model') }}:</dt>
+                <dt>{{ $t('pageInventory.table.bmcManagerModel') }}:</dt>
                 <dd>{{ dataFormatter(item.model) }}</dd>
-                <!-- UUID -->
-                <dt>{{ $t('pageInventory.table.uuid') }}:</dt>
-                <dd>{{ dataFormatter(item.uuid) }}</dd>
-                <!-- Service entry point UUID -->
-                <dt>{{ $t('pageInventory.table.serviceEntryPointUuid') }}:</dt>
-                <dd>{{ dataFormatter(item.serviceEntryPointUuid) }}</dd>
-              </dl>
-            </b-col>
-            <b-col class="mt-2" sm="6" xl="6">
-              <dl>
-                <!-- Status state -->
-                <dt>{{ $t('pageInventory.table.statusState') }}:</dt>
-                <dd>{{ dataFormatter(item.statusState) }}</dd>
-                <!-- Power state -->
-                <dt>{{ $t('pageInventory.table.power') }}:</dt>
-                <dd>{{ dataFormatter(item.powerState) }}</dd>
-                <!-- Health rollup -->
-                <dt>{{ $t('pageInventory.table.healthRollup') }}:</dt>
-                <dd>{{ dataFormatter(item.healthRollup) }}</dd>
-                <!-- BMC date and time -->
-                <dt>{{ $t('pageInventory.table.bmcDateTime') }}:</dt>
-                <dd>
-                  {{ item.dateTime | formatDate }}
-                  {{ item.dateTime | formatTime }}
-                </dd>
-                <!-- Reset date and time -->
-                <dt>{{ $t('pageInventory.table.lastResetTime') }}:</dt>
-                <dd>
-                  {{ item.lastResetTime | formatDate }}
-                  {{ item.lastResetTime | formatTime }}
-                </dd>
-              </dl>
-            </b-col>
-          </b-row>
-          <div class="section-divider mb-3 mt-3"></div>
-          <b-row>
-            <b-col class="mt-2" sm="6" xl="6">
-              <dl>
-                <!-- Manufacturer -->
-                <dt>{{ $t('pageInventory.table.manufacturer') }}:</dt>
-                <dd>{{ dataFormatter(item.manufacturer) }}</dd>
-                <!-- Description -->
-                <dt>{{ $t('pageInventory.table.description') }}:</dt>
-                <dd>{{ dataFormatter(item.description) }}</dd>
-                <!-- Manager type -->
-                <dt>{{ $t('pageInventory.table.managerType') }}:</dt>
-                <dd>{{ dataFormatter(item.managerType) }}</dd>
-              </dl>
-            </b-col>
-            <b-col class="mt-2" sm="6" xl="6">
-              <!-- Firmware Version  -->
-              <dl>
-                <dt>{{ $t('pageInventory.table.firmwareVersion') }}:</dt>
-                <dd>{{ item.firmwareVersion }}</dd>
-              </dl>
-              <!-- Graphical console -->
-              <p class="mt-1 mb-2 h6 float-none m-0">
-                {{ $t('pageInventory.table.graphicalConsole') }}
-              </p>
-              <dl class="ml-4">
-                <dt>{{ $t('pageInventory.table.connectTypesSupported') }}:</dt>
-                <dd>
-                  {{ dataFormatterArray(item.graphicalConsoleConnectTypes) }}
-                </dd>
-                <dt>{{ $t('pageInventory.table.maxConcurrentSessions') }}:</dt>
-                <dd>
-                  {{ dataFormatter(item.graphicalConsoleMaxSessions) }}
-                </dd>
-                <dt>{{ $t('pageInventory.table.serviceEnabled') }}:</dt>
-                <dd>
-                  {{ dataFormatter(item.graphicalConsoleEnabled) }}
-                </dd>
-              </dl>
-              <!-- Serial console -->
-              <p class="mt-1 mb-2 h6 float-none m-0">
-                {{ $t('pageInventory.table.serialConsole') }}
-              </p>
-              <dl class="ml-4">
-                <dt>{{ $t('pageInventory.table.connectTypesSupported') }}:</dt>
-                <dd>
-                  {{ dataFormatterArray(item.serialConsoleConnectTypes) }}
-                </dd>
-                <dt>{{ $t('pageInventory.table.maxConcurrentSessions') }}:</dt>
-                <dd>{{ dataFormatter(item.serialConsoleMaxSessions) }}</dd>
-                <dt>{{ $t('pageInventory.table.serviceEnabled') }}:</dt>
-                <dd>{{ dataFormatter(item.serialConsoleEnabled) }}</dd>
               </dl>
             </b-col>
           </b-row>
