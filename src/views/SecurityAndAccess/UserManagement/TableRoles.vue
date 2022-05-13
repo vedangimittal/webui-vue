1fbbcb75143bd025a6b13140e17a81e08ac92713
Kenneth Fullbright <Kenneth.Fullbright@ibm.com>
2022-05-13 12:40:20 -0500

    Removed Operator and NoAccess roles from desciption table (#228) [SW550558]
    Operator and NoAccess role infomation shouldn't be
    in the description table
    
    - Erased Operator and NoAccess role and related information
    form the description table
    
    Signed-off-by: Kenneth Fullbright <kenneth.fullbright@ibm.com>

--- src/views/SecurityAndAccess/UserManagement/TableRoles.vue
+++ src/views/SecurityAndAccess/UserManagement/TableRoles.vue
@@ -38,53 +38,41 @@ export default {
             'pageUserManagement.tableRoles.configureComponentsManagedByThisService'
           ),
           administrator: true,
-          operator: true,
           readonly: false,
-          noaccess: false,
         },
         {
           description: this.$t(
             'pageUserManagement.tableRoles.configureManagerResources'
           ),
           administrator: true,
-          operator: false,
           readonly: false,
-          noaccess: false,
         },
         {
           description: this.$t(
             'pageUserManagement.tableRoles.updatePasswordForCurrentUserAccount'
           ),
           administrator: true,
-          operator: true,
           readonly: true,
-          noaccess: false,
         },
         {
           description: this.$t(
             'pageUserManagement.tableRoles.configureUsersAndTheirAccounts'
           ),
           administrator: true,
-          operator: false,
           readonly: false,
-          noaccess: false,
         },
         {
           description: this.$t(
             'pageUserManagement.tableRoles.logInToTheServiceAndReadResources'
           ),
           administrator: true,
-          operator: true,
           readonly: true,
-          noaccess: false,
         },
       ],
       fields: [
         { key: 'description', label: 'Privilege' },
         { key: 'administrator', label: 'Administrator', class: 'text-center' },
-        { key: 'operator', label: 'Operator', class: 'text-center' },
         { key: 'readonly', label: 'ReadOnly', class: 'text-center' },
-        { key: 'noaccess', label: 'NoAccess', class: 'text-center' },
       ],
     };
   },
