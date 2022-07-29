52e583ced043050f8b9e124fea5e24ab121132ff
Kenneth Fullbright <Kenneth.Fullbright@ibm.com>
2022-07-29 01:22:45 -0500

    Created real time indicator postCodeValue filter (#302)
    * Created real time indicator postCodeValue filter
    
    Real time indicators should not show any strings
    with human unreadable characters and all 0
    
    - Put in logic to filter out strings that
    are only 0s
    
    - Put in logic to filter out postCodeValues
    that isn't alphanumeric
    
    Signed-off-by: Kenneth Fullbright <kenneth.fullbright@ibm.com>
    
    * refactored the filter and added increment code

--- src/views/Logs/PostCodeLogs/PostCodes.vue
+++ src/views/Logs/PostCodeLogs/PostCodes.vue
@@ -7,19 +7,59 @@
 <script>
 export default {
   name: 'PostCodes',
+  data() {
+    return {
+      previousPostCode: '',
+    };
+  },
   computed: {
     postCodeValue() {
       return this.$store.getters['global/postCodeValue'];
     },
-    postCodes() {
-      if (this.postCodeValue) {
-        const { U64 } = require('n64');
-        let big = U64(this.postCodeValue, 10);
-        big.toString();
-        let bytearray = big.toLE(Buffer);
-        var finalConvertedCode = bytearray.toString();
-        return finalConvertedCode;
-      } else return '';
+    postCodes: {
+      get() {
+        const value = () => {
+          if (this.postCodeValue) {
+            const { U64 } = require('n64');
+            let big = U64(this.postCodeValue, 10);
+            big.toString();
+            let bytearray = big.toLE(Buffer);
+            let finalConvertedCode = bytearray.toString();
+            // Filter out all non readable values and all 00000000
+            // Yes, whitespaces for 'STANDBY ' and 'RUNTIME ' must be there
+            if (
+              finalConvertedCode === 'STANDBY ' ||
+              finalConvertedCode === 'RUNTIME ' ||
+              (finalConvertedCode != '00000000' &&
+                /^[a-z0-9]+$/i.test(finalConvertedCode))
+            ) {
+              this.previousPostCode = finalConvertedCode;
+              return finalConvertedCode;
+            } else {
+              return this.previousPostCode;
+            }
+          } else {
+            return '';
+          }
+        };
+        return value();
+      },
+      set(value = '') {
+        // Default value is an empty string
+        // If the last character of the string value is a number
+        // and that number isn't 9, then increment the value by 1
+        if (
+          /^[0-9]+$/i.test(value[value?.length - 1]) &&
+          value[value?.length - 1] != '9'
+        ) {
+          // Take out the last character of the string
+          // Increment the last character of the string by 1
+          // Put the beginning and the ending of the string back together.
+          // ex: '0132EDD5' will turn to '0132EDD6'
+          this.previousPostCode =
+            value.slice(0, -1) + (Number(value[value.length - 1]) + 1);
+        }
+      },
     },
   },
 };
