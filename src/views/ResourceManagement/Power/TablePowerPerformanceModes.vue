1e52d284ea0fa23e4d63b35a078bbf8a7f08d9d4
Sandeepa Singh <Sandeepa.Singh@ibm.com>
2022-10-10 15:16:35 +0530

    Fix power page translation bug (#361)

--- src/views/ResourceManagement/Power/TablePowerPerformanceModes.vue
+++ src/views/ResourceManagement/Power/TablePowerPerformanceModes.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-table stacked="sm" hover :items="items"> </b-table>
+  <b-table stacked="sm" hover :items="items" :fields="fields"> </b-table>
 </template>
 
 <script>
@@ -7,17 +7,29 @@ export default {
   components: {},
   data() {
     return {
+      fields: [
+        {
+          key: 'setting',
+          label: this.$t('pagePower.tableRoles.setting'),
+          sortable: false,
+        },
+        {
+          key: 'description',
+          label: this.$t('pagePower.tableRoles.description'),
+          sortable: false,
+        },
+      ],
       items: [
         {
-          setting: 'Static',
+          setting: this.$t('pagePower.selectMode.static'),
           description: this.$t('pagePower.tableRoles.descStatic'),
         },
         {
-          setting: 'Power saving',
+          setting: this.$t('pagePower.selectMode.powerSaving'),
           description: this.$t('pagePower.tableRoles.descPowerSaving'),
         },
         {
-          setting: 'Maximum performance',
+          setting: this.$t('pagePower.selectMode.maximumPerformance'),
           description: this.$t('pagePower.tableRoles.descMaximumPerformance'),
         },
       ],
