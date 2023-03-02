801e00a83dbb536a8319f6d57d44762c22ba1335
Nikhil Ashoka <96164786+Nikhil-Ashoka@users.noreply.github.com>
2023-03-03 03:18:50 +0530

    Checking IP address and filtering (#83)
    - Checking if ClientOriginIPAddress includes additional characters. If so, we still filter them out and display
      the IP address. Else would display the complete original ClientOriginIPAddress.
    
    Signed-off-by: Nikhil Ashoka <a.nikhil@ibm.com>

--- src/store/modules/SecurityAndAccess/SessionsStore.js
+++ src/store/modules/SecurityAndAccess/SessionsStore.js
@@ -26,9 +26,11 @@ const SessionsStore = {
         .then((sessionUris) => {
           const allConnectionsData = sessionUris.map((sessionUri) => {
             //For filtering IP address to IPv4
-            let filteredIPAddress = sessionUri.data?.ClientOriginIPAddress.slice(
-              7
-            );
+            let filteredIPAddress = sessionUri.data?.ClientOriginIPAddress.includes(
+              '::ffff'
+            )
+              ? sessionUri.data?.ClientOriginIPAddress.slice(7)
+              : sessionUri.data?.ClientOriginIPAddress;
             return {
               clientID: sessionUri.data?.Oem?.OpenBMC.ClientID,
               username: sessionUri.data?.UserName,
