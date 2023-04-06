d005a954b28f17ca85926c3670b201ea1f6d4415
Nikhil Ashoka <96164786+Nikhil-Ashoka@users.noreply.github.com>
2023-04-06 14:07:55 +0530

    VET Capabilties names updated (#92)
    * VET Capabilties names updated
    
    - VET Capabilities names were empty because some values names were new and was not taken from the translation file,
    So, Added new name and also taking the direct value from the redfish response if the name does not match.
    
    - Defect: https://jazz07.rchland.ibm.com:13443/jazz/web/projects/CSSD#action=com.ibm.team.workitem.viewWorkItem&id=444004
    
    Signed-off-by: Nikhil Ashoka <a.nikhil@ibm.com>
    
    * Update AppHeader.spec.js.snap
    
    ---------
    
    Signed-off-by: Nikhil Ashoka <a.nikhil@ibm.com>

--- src/views/ResourceManagement/CapacityOnDemand/CapacityOnDemandTable.vue
+++ src/views/ResourceManagement/CapacityOnDemand/CapacityOnDemandTable.vue
@@ -17,6 +17,15 @@
             <div v-else-if="value == 'AIX Diagnostics'">
               {{ $t('pageCapacityOnDemand.setting.aixDiagnostics') }}
             </div>
+            <div v-else-if="value == 'AIX Update Access Key'">
+              {{ $t('pageCapacityOnDemand.setting.aixUpdateAccessKey') }}
+            </div>
+            <div v-else-if="value == 'Asset Protection Machine ID'">
+              {{ $t('pageCapacityOnDemand.setting.assetProtectionMachineID') }}
+            </div>
+            <div v-else-if="value == 'Asset Protection Public Key'">
+              {{ $t('pageCapacityOnDemand.setting.assetProtectionPublicKey') }}
+            </div>
             <div v-else-if="value == 'Active memory expansion'">
               {{ $t('pageCapacityOnDemand.setting.activeMemoryExpansion') }}
             </div>
@@ -25,6 +34,12 @@
             </div>
             <div v-else-if="value == 'CAPI'">
               {{ $t('pageCapacityOnDemand.setting.capi') }}
+            </div>
+            <div v-else-if="value == 'Elastic MemoryGB*Days Available'">
+              {{ $t('pageCapacityOnDemand.setting.elasticMemoryGBAvailable') }}
+            </div>
+            <div v-else-if="value == 'Elastic Processor*Days Available'">
+              {{ $t('pageCapacityOnDemand.setting.elasticProcessorAvailable') }}
             </div>
             <div v-else-if="value == 'IBMi 100% interactive capacity'">
               {{ $t('pageCapacityOnDemand.setting.interactiveCapacity') }}
@@ -46,8 +61,47 @@
             >
               {{ $t('pageCapacityOnDemand.setting.perPartition') }}
             </div>
+            <div v-else-if="value == 'P05 Virtual Tier'">
+              {{ $t('pageCapacityOnDemand.setting.p05VirtualTier') }}
+            </div>
+            <div v-else-if="value == 'P10 Virtual Tier'">
+              {{ $t('pageCapacityOnDemand.setting.p10VirtualTier') }}
+            </div>
+            <div v-else-if="value == 'P20 Virtual Tier'">
+              {{ $t('pageCapacityOnDemand.setting.p20VirtualTier') }}
+            </div>
+            <div v-else-if="value == 'P30 Virtual Tier'">
+              {{ $t('pageCapacityOnDemand.setting.p30VirtualTier') }}
+            </div>
+            <div v-else-if="value == 'Permanent Memory Licenses (GB)'">
+              {{ $t('pageCapacityOnDemand.setting.permanentMemoryLicenses') }}
+            </div>
+            <div v-else-if="value == 'Permanent Processor Licenses'">
+              {{
+                $t('pageCapacityOnDemand.setting.permanentProcessorLicenses')
+              }}
+            </div>
             <div v-else-if="value == 'Subprocessor partitioning'">
               {{ $t('pageCapacityOnDemand.setting.subprocessorPartitioning') }}
+            </div>
+            <div v-else-if="value == 'System Anchor'">
+              {{ $t('pageCapacityOnDemand.setting.systemAnchor') }}
+            </div>
+            <div v-else-if="value == 'Trial Memory Licenses (GB)'">
+              {{ $t('pageCapacityOnDemand.setting.trialMemoryLicenses') }}
+            </div>
+            <div v-else-if="value == 'Trial Processor Licenses'">
+              {{ $t('pageCapacityOnDemand.setting.trialProcessorLicenses') }}
+            </div>
+            <div v-else-if="value == 'Firmware Update Access Key'">
+              {{ $t('pageCapacityOnDemand.setting.firmwareUpdateAccessKey') }}
+            </div>
+            <div v-else-if="value == 'Virtualization Engine Technology'">
+              {{
+                $t(
+                  'pageCapacityOnDemand.setting.virtualizationEngineTechnology'
+                )
+              }}
             </div>
             <div v-else-if="value == 'Virtual I/O server capable'">
               {{ $t('pageCapacityOnDemand.setting.virtualIoServer') }}
@@ -55,6 +109,7 @@
             <div v-else-if="value == 'Virtual tiers allowed'">
               {{ $t('pageCapacityOnDemand.setting.virtualTiersAllowed') }}
             </div>
+            <div v-else>{{ value }}</div>
           </template>
           <!-- Status column -->
           <template #cell(status)="{ value }">
