9f3ae79bd59d0a4c5867ceef744d171edefd352e
Nabil Ananthamangalath <nabilmanjeri@gmail.com>
2024-01-25 20:00:27 +0530

    Implemented Redfish message ID checks using Regex (#170)
    - Replaced the hardcoded checks for message IDs with Regular Expressions. Implemented the possible message IDs using regex in a separate module(src/utilities/GlobalConstants.js)
    
    - JIRA: https://jsw.ibm.com/browse/PFEBMC-1934
    
    - Defect: https://jazz07.rchland.ibm.com:13443/jazz/web/projects/CSSD#action=com.ibm.team.workitem.viewWorkItem&id=588634
    
    Signed-off-by: Nabil Ananthamangalath <nabilmanjeri@gmail.com>

---
+++ src/utilities/GlobalConstants.js
@@ -0,0 +1,10 @@
+export const REGEX_MAPPINGS = Object.freeze({
+  resourceInStandby: /Base\.(\d+\.){3}ResourceInStandby/,
+  actionParameterUnknown: /Base\.(\d+\.){3}ActionParameterUnknown/,
+  resourceAtUriUnauthorized: /Base\.(\d+\.){3}ResourceAtUriUnauthorized/,
+  insufficientPrivilege: /Base\.(\d+\.){3}InsufficientPrivilege/,
+  resourceInUse: /Base\.(\d+\.){3}ResourceInUse/,
+  propertyValueFormatError: /Base\.(\d+\.){3}PropertyValueFormatError/,
+  createLimitReachedForResource: /Base\.(\d+\.){3}CreateLimitReachedForResource/,
+  resourceCannotBeDeleted: /Base\.(\d+\.){3}ResourceCannotBeDeleted/,
+});
