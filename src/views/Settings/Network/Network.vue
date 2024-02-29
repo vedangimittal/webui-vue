748f8add8c8a726160fe8954b2c27df85d305a5d
Steffi Antony <125690789+SteffiAntony@users.noreply.github.com>
2024-02-29 19:41:26 +0530

    Handled DHCP network configurations (#193)
    - When DHCP is enabled on eth0, eth1 is also getting enabled with DHCP & its network configurations are lost
    - Defect: https://jazz07.rchland.ibm.com:13443/jazz/web/projects/CSSD#action=com.ibm.team.workitem.viewWorkItem&id=598793
    Signed-off-by: Steffi Antony <“steffiantony196@gmail.com”>
    
    Co-authored-by: Steffi Antony <“steffiantony196@gmail.com”>

--- src/views/Settings/Network/Network.vue
+++ src/views/Settings/Network/Network.vue
@@ -105,6 +105,7 @@ export default {
       this.subnet = item.SubnetMask;
       this.ipAddress = item.Address;
     });
+    this.$store.dispatch('network/setSelectedTabIndex', 0);
   },
   created() {
     this.startLoader();
