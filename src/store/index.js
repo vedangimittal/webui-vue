5abc3b56eab8fe3e955700c9bddb4316af291a36
vedangimittal <69679625+vedangimittal@users.noreply.github.com>
2024-12-05 19:57:19 +0530

    Implemented System Parameters Page (#310)
    - Implemented System Parameters Page - 1060
    - Jira Story: https://jsw.ibm.com/browse/PFEBMC-2390
    
    Signed-off-by: Vedangi Mittal <vedangimittal3004@gmail.com>

--- src/store/index.js
+++ src/store/index.js
@@ -25,6 +25,7 @@ import FanStore from './modules/HardwareStatus/FanStore';
 import MemoryStore from './modules/HardwareStatus/MemoryStore';
 import AssemblyStore from './modules/HardwareStatus/AssemblyStore';
 import FabricAdaptersStore from './modules/HardwareStatus/FabricAdaptersStore';
+import SystemParametersStore from './modules/ResourceManagement/SystemParametersStore';
 import CertificatesStore from './modules/SecurityAndAccess/CertificatesStore';
 import UserManagementStore from './modules/SecurityAndAccess/UserManagementStore';
 
@@ -56,6 +57,7 @@ export {
   BmcStore,
   AssemblyStore,
   FabricAdaptersStore,
+  SystemParametersStore,
   CertificatesStore,
   UserManagementStore,
 };
